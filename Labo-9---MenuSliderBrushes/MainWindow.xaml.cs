3c428d1644810af25a07ef5fa471ce658e929cb2
FJanssen92 <12402449@student.pxl.be>
2024-11-15 08:49:46 +0100

    labo9 klaar

--- Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
+++ Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
@@ -20,5 +20,50 @@ namespace Labo_9___MenuSliderBrushes
         {
             InitializeComponent();
         }
+
+        private void closeMenu_Click(object sender, RoutedEventArgs e)
+        {
+            var answer =  MessageBox.Show("Wilt u de applicatie afsluiten?", "Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (answer == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+            
+                
+        }
+
+        private void firstNumberMenu_Click(object sender, RoutedEventArgs e)
+        {
+            numberOneTextBox.Text = "2";
+        }
+
+        private void secondNumberMenu_Click(object sender, RoutedEventArgs e)
+        {
+            numberTwoTextBox.Text = "2";
+        }
+
+        private void firstNumberSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberOneTextBox.Text = firstNumberSlider.Value.ToString();
+        }
+
+        private void secondNumberSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberTwoTextBox.Text = secondNumberSlider.Value.ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender == numberOneTextBox)
+            {
+                firstNumberSlider.Value = double.Parse(numberOneTextBox.Text);
+            }
+
+            else if(sender == numberTwoTextBox)
+            {
+                secondNumberSlider.Value = double.Parse(numberTwoTextBox.Text);
+            }
+        }
     }
 }
